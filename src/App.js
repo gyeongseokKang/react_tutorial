59614831d47fefe12e092fa194689c5cde5a332a
kyeungseok_kang <kyeungseok_kang@tmax.co.kr>
2020-10-18 17:37:32 +0900

    [Study] 3.6 까지

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 import MyComponent from './MyComponent';
+import Counter from './Counter';
+import Say from './Say';
 
 const App = () => {
-	return <MyComponent name="React" favoriteNumber={1}>리액트</MyComponent>
+	return <Say />;
 }
 
 export default App;
