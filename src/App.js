30d4dc53da75e391604a62628210eb05d2002374
kyeungseok_kang <kyeungseok_kang@tmax.co.kr>
2020-10-18 19:57:13 +0900

    [Study] 4장 마무리

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import './App.css';
 import MyComponent from './MyComponent';
 import Counter from './Counter';
 import Say from './Say';
+import EventPractice from './EventPractice';
 
 const App = () => {
-	return <Say />;
+	return <EventPractice />;
 }
 
 export default App;
