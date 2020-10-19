8fbaf048f4534639850dd10c7a611a473d4db8ee
kyeungseok_kang <kyeungseok_kang@tmax.co.kr>
2020-10-19 19:44:16 +0900

    [Study] 5장 마무리

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
+/*
 import MyComponent from './MyComponent';
 import Counter from './Counter';
 import Say from './Say';
 import EventPractice from './EventPractice';
+import ValidationSample from './ValidationSample';
+*/
+import ScrollBox from './ScrollBox';
 
-const App = () => {
-	return <EventPractice />;
+class App extends Component {
+	render() {
+		return (
+			<div>
+				<ScrollBox ref={(ref) => this.scrollBox = ref}></ScrollBox>
+				<button onClick={() => this.scrollBox.scrollToBottom()}>맨 밑으로</button>
+			</div>
+		)
+	}
 }
+/*
+const App = () => {
+	return (
+		<div>
+			<ScrollBox ref={(ref) => console.log(ref, this)}></ScrollBox>
+			<button onClick={() => this.scrollBox.scrollToBottom()}>맨 밑으로</button>
+		</div>
+	)
+}*/
 //git teet
 export default App;
