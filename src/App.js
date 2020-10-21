de6e41d0769f6cd3ad214d160d0b34b2cfa283c0
kyeungseok_kang <kyeungseok_kang@tmax.co.kr>
2020-10-21 20:42:17 +0900

    [Study] 7장 마무리

--- src/App.js
+++ src/App.js
@@ -7,13 +7,29 @@ import Say from './Say';
 import EventPractice from './EventPractice';
 import ValidationSample from './ValidationSample';
 import ScrollBox from './ScrollBox';
-*/
 import IterationSample from './IterationSample';
+*/
+import LifeCycleSample from './LifeCycleSample';
 
+function getRandomColor() {
+	return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 class App extends Component {
+	state = {
+		color: '#000000'
+	}
+
+	handleClick = () => {
+		this.setState({
+			color: getRandomColor()
+		});
+	}
 	render() {
 		return (
-			<IterationSample />
+			<div>
+				<button onClick={this.handleClick}>random color</button>
+				<LifeCycleSample color={this.state.color} />
+			</div>
 		)
 	}
 }
