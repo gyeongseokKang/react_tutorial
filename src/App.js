ec35abf37cc4d5c618a75e51a3f6ea6d3f6829a3
kyeungseok_kang <kyeungseok_kang@tmax.co.kr>
2020-10-22 20:46:51 +0900

    [New Project] ToDo app

--- src/App.js
+++ src/App.js
@@ -1,38 +1,6 @@
-import React, { Component } from 'react';
-import './App.css';
+import React from 'react';
 
-import Info from './basic_react/Info';
-
-/*
-function getRandomColor() {
-	return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
-class App extends Component {
-	state = {
-		color: '#000000'
-	}
-
-	handleClick = () => {
-		this.setState({
-			color: getRandomColor()
-		});
-	}
-	render() {
-		return (
-			<div>
-				<button onClick={this.handleClick}>random color</button>
-				<LifeCycleSample color={this.state.color} />
-			</div>
-		)
-	}
-}
-*/
 const App = () => {
-	return (
-		<div>
-			<Info />
-		</div>
-	)
+	return <div> make a ToDo App with React</div>
 }
-
 export default App;
