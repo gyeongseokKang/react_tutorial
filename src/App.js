d8047d0d1b1291c1c4b4dfc74a2e471433e70d59
kyeungseok_kang <kyeungseok_kang@tmax.co.kr>
2020-10-27 20:45:29 +0900

    [New Feature] todo app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,51 @@
-import React from 'react';
+import React, { useState, useRef, useCallback } from 'react';
+import TodoTemplate from './components/TodoTemplate';
+import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
+
 
 const App = () => {
-	return <div> make a ToDo App with React</div>
+	const [todos, setTodos] = useState([
+		{
+			id: 1,
+			text: 'react 기초',
+			checked: true
+		},
+		{
+			id: 2,
+			text: 'component 수정',
+			checked: true
+		},
+		{
+			id: 3,
+			text: '일정 관리 앱 만들어보기',
+			checked: false
+		},
+	]);
+
+	const nextId = useRef(4);
+	const onInsert = useCallback(
+		text => {
+			const todo = {
+				id: nextId.current,
+				text: text,
+				checked: false,
+			};
+			setTodos(todos.concat(todo));
+			nextId.current += 1;
+		},
+		[todos]
+	);
+
+	const onRemove = useCallback(
+		id => {
+			setTodos(todos.filter(todo => todo.id !== id));
+		}, [todos]);
+
+	return (
+		<TodoTemplate>
+			<TodoInsert onInsert={onInsert} />
+			<TodoList todos={todos} onRemove={onRemove} />
+		</TodoTemplate>)
 }
 export default App;
