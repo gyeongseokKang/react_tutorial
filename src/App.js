c32c538776f7292f319ef13e08356bbabcda3d30
kyeungseok_kang <kyeungseok_kang@tmax.co.kr>
2020-11-13 13:25:02 +0900

    [New Feature] news app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import axios from 'axios';
+import NewsList from './NewsList';
+import Categories from './Categories';
 
 const App = () => {
-	const [data, setData] = useState(null);
-	const onClick = () => {
-		axios.get('https://jsonplaceholder.typicode.com/todos/1').then(response => {
-			setData(response.data);
-		});
-	};
+	const [category, setCategory] = useState('all');
+	const onSelect = useCallback(category => setCategory(category), []);
+
 	return (
-		<div>
-			<div>
-				<button onClick={onClick}>불러오기</button>
-			</div>
-			{data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true}></textarea>}
-		</div>
-	);
+		<>
+			<Categories category={category} onSelect={onSelect} />
+			<NewsList category={category} />
+		</>
+	)
 };
 export default App;
---
+++ src/NewsList.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import NewsItem from './NewsItem';
+import axios from 'axios';
+import Profile from './router_tutorial/Profile';
+
+const NewsListBlock = styled.div`
+	box-sizing : border-box;
+	padding-bottom : 3rem;
+	width : 768px;
+	margin : 0 auto;
+	margin-top : 2rem;
+	@media screen and (max-width :768px) {
+		width : 100%;
+		padding-left : 1rem;
+		padding-right : 1rem;
+	}
+`;
+
+const sampleArticle = {
+	title: 'title',
+	description: 'content',
+	url: 'https://google.com',
+	urlToImage: 'https://via.placeholder.com/160'
+};
+
+const NewsList = ({ category }) => {
+	const [articles, setArticles] = useState(null);
+	const [loading, setLoading] = useState(null);
+	const APP_KEY = '16f39cd4f90340028a1fe0dfae7d4453';
+	useEffect(() => {
+		const fetchData = async () => {
+			setLoading(true);
+			try {
+				const query = category === 'all' ? '' : `&category=${category}`;
+				const response = await axios.get(`http://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=${APP_KEY}`);
+				setArticles(response.data.articles);
+			} catch (e) {
+				console.log(e);
+			}
+			setLoading(false);
+		};
+
+		fetchData();
+	}, [category]);
+
+	if (loading) {
+		return <NewsListBlock> loading... </NewsListBlock>;
+	}
+
+	if (!articles) {
+		return null;
+	}
+
+
+	return (
+		<NewsListBlock>
+			{articles.map(article => (
+				<NewsItem key={article.url} article={article} />
+			))}
+		</NewsListBlock>
+	);
+};
+
+export default NewsList;
